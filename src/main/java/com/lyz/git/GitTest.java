315e7ec6825ba93377814ee844ebc717b8f29897
LiangYuZhu <2672737057@qq.com>
2022-10-25 00:11:39 +0800

    first commit

---
+++ src/main/java/com/lyz/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lyz.git;
+
+/**
+ * @author 梁**
+ * @create 2022-10-25 0:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("贾雨村");
+    }
+}
